7caac64cb10fc9df89f074df88add5f5dfd4213a
Gerrit Holz <gerrit1986@gmail.com>
2013-01-21 15:45:50 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,25 +1,42 @@
 <?php
+mb_internal_encoding('UTF-8');
 $host = 'host';
 $user = 'user';
 $password = 'password';
 $database = 'database';
-
+  
+# get credentials, connect to database server and forget password
 $con = mysql_connect($host, $user, $password);
+unset($password);
 
+# check connection to database server
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
 
-mysql_select_db($database, $con) or die('Error while selecting: ' . mysql_error() . ' Error No.: '. mysql_errno()); 
+# select database
+mysql_select_db($database, $con) or die('Error while selecting: ' . mysql_error() . ' Error No.: '. mysql_errno());
 
-$answers = "INSERT INTO Answers (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9, Item10, Item11, Item12, Item13, Item14, Item15, Item16, Item17, Item18, Item19, Item20) VALUES ('$_POST[Item1]','$_POST[Item2]','$_POST[Item3]','$_POST[Item4]','$_POST[Item5]','$_POST[Item6]','$_POST[Item7]','$_POST[Item8]','$_POST[Item9]','$_POST[Item10]','$_POST[Item11]','$_POST[Item12]','$_POST[Item13]','$_POST[Item14]','$_POST[Item15]','$_POST[Item16]','$_POST[Item17]','$_POST[Item18]','$_POST[Item19]','$_POST[Item20]');";
-
-if (!mysql_query($answers, $con))
+# check whether the UEQ has already been submitted
+if ($_COOKIE['submitted'] == 'True')
   {
-  die('Error while inserting: ' . mysql_error() . ' Error No.: '. mysql_errno());
+  die("Sie hatten den Fragebogen bereits abgegeben. Ihre erste Abgabe wird berücksichtigt. Vielen Dank!");
   }
+
+# define insertion and insert to database
+$insertion = "INSERT INTO Answers (Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9, Item10, Item11, Item12, Item13, Item14, Item15, Item16, Item17, Item18, Item19, Item20) VALUES ('$_POST[Item1]','$_POST[Item2]','$_POST[Item3]','$_POST[Item4]','$_POST[Item5]','$_POST[Item6]','$_POST[Item7]','$_POST[Item8]','$_POST[Item9]','$_POST[Item10]','$_POST[Item11]','$_POST[Item12]','$_POST[Item13]','$_POST[Item14]','$_POST[Item15]','$_POST[Item16]','$_POST[Item17]','$_POST[Item18]','$_POST[Item19]','$_POST[Item20]');";
+
+if (!mysql_query($insertion, $con))
+  {
+  die("Error while inserting: " . mysql_error() . " Error No.: ". mysql_errno());
+  }
+
+# set cookie to prevent multiple submissions
+setcookie('submitted', 'True', time()+86400);
 echo "Vielen Dank für Ihre Teilnahme!";
 
+# close connection
 mysql_close($con);
+}
 ?>
